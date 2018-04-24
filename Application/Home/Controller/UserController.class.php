cab33df49f878db4a14bb11958fe11370315bc67
bilibili丶教主 <534889865@qq.com>
2018-04-24 11:11:53 +0800

    儿童生活娱乐信息平台 v1.0

---
+++ Application/Home/Controller/UserController.class.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Home\Controller;
+
+use Think\Controller;
+
+class UserController extends BaseController
+{
+    public function userCenter(){
+        $this->display();
+    }
+}
