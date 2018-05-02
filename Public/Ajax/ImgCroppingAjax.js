371005932495ed542c859e27bf35569395605132
bilibili丶教主 <534889865@qq.com>
2018-05-02 15:23:39 +0800

    儿童生活娱乐信息平台 v1.0

--- Public/Ajax/ImgCroppingAjax.js
+++ Public/Ajax/ImgCroppingAjax.js
@@ -2,42 +2,51 @@
 (window.onresize = function () {
     var win_height = $(window).height();
     var win_width = $(window).width();
-    if (win_width <= 768){
+    if (win_width <= 768) {
         $(".tailoring-content").css({
-            "top": (win_height - $(".tailoring-content").outerHeight())/2,
+            "top": (win_height - $(".tailoring-content").outerHeight()) / 2,
             "left": 0
         });
-    }else{
+    } else {
         $(".tailoring-content").css({
-            "top": (win_height - $(".tailoring-content").outerHeight())/2,
-            "left": (win_width - $(".tailoring-content").outerWidth())/2
+            "top": (win_height - $(".tailoring-content").outerHeight()) / 2,
+            "left": (win_width - $(".tailoring-content").outerWidth()) / 2
         });
     }
 })();
 
 //弹出图片裁剪框
-$("#replaceImg").on("click",function () {
+$("#replaceImg_small").on("click", function () {
+    image_state = 1;
+    $('#tailoringImg').cropper('setAspectRatio', 1 / 1, false);
     $(".tailoring-container").toggle();
 });
+$("#replaceImg_middle").on("click", function () {
+    image_state = 2;
+    $('#tailoringImg').cropper('setAspectRatio', 2 / 1, false);
+    $(".tailoring-container").toggle();
+});
+
 //图像上传
 function selectImg(file) {
-    if (!file.files || !file.files[0]){
+    if (!file.files || !file.files[0]) {
         return;
     }
     var reader = new FileReader();
     reader.onload = function (evt) {
         var replaceSrc = evt.target.result;
         //更换cropper的图片
-        $('#tailoringImg').cropper('replace', replaceSrc,false);//默认false，适应高度，不失真
-    }
+        $('#tailoringImg').cropper('replace', replaceSrc, false);//默认false，适应高度，不失真
+    };
     reader.readAsDataURL(file.files[0]);
 }
+
 //cropper图片裁剪
 $('#tailoringImg').cropper({
-    aspectRatio: 1/1,//默认比例
+    aspectRatio: 1 / 1,//默认比例
     preview: '.previewImg',//预览视图
-    guides: false,  //裁剪框的虚线(九宫格)
-    autoCropArea: 0.5,  //0-1之间的数值，定义自动剪裁区域的大小，默认0.8
+    guides: true,  //裁剪框的虚线(九宫格)
+    autoCropArea: 0.8,  //0-1之间的数值，定义自动剪裁区域的大小，默认0.8
     movable: false, //是否允许移动图片
     dragCrop: true,  //是否允许移除当前的剪裁框，并通过拖动来新建一个剪裁框区域
     movable: true,  //是否允许移动剪裁框
@@ -46,25 +55,26 @@ $('#tailoringImg').cropper({
     mouseWheelZoom: false,  //是否允许通过鼠标滚轮来缩放图片
     touchDragZoom: true,  //是否允许通过触摸移动来缩放图片
     rotatable: true,  //是否允许旋转图片
-    crop: function(e) {
+    crop: function (e) {
         // 输出结果数据裁剪图像。
     }
 });
+
 //旋转
-$(".cropper-rotate-btn").on("click",function () {
+$(".cropper-rotate-btn").on("click", function () {
     $('#tailoringImg').cropper("rotate", 45);
 });
 //复位
-$(".cropper-reset-btn").on("click",function () {
+$(".cropper-reset-btn").on("click", function () {
     $('#tailoringImg').cropper("reset");
 });
 //换向
 var flagX = true;
-$(".cropper-scaleX-btn").on("click",function () {
-    if(flagX){
+$(".cropper-scaleX-btn").on("click", function () {
+    if (flagX) {
         $('#tailoringImg').cropper("scaleX", -1);
         flagX = false;
-    }else{
+    } else {
         $('#tailoringImg').cropper("scaleX", 1);
         flagX = true;
     }
@@ -72,18 +82,22 @@ $(".cropper-scaleX-btn").on("click",function () {
 });
 
 //裁剪后的处理
-$("#sureCut").on("click",function () {
-    if ($("#tailoringImg").attr("src") == null ){
+$("#sureCut").on("click", function () {
+    if ($("#tailoringImg").attr("src") == null) {
         return false;
-    }else{
+    } else {
         var cas = $('#tailoringImg').cropper('getCroppedCanvas');//获取被裁剪后的canvas
         var base64url = cas.toDataURL('image/png'); //转换为base64地址形式
-        $("#finalImg").prop("src",base64url);//显示为图片的形式
-
+        if (image_state == 1) {
+            $("#finalImg_small").prop("src", base64url);//显示为图片的形式
+        } else if (image_state == 2) {
+            $("#finalImg_middle").prop("src", base64url);//显示为图片的形式
+        }
         //关闭裁剪框
         closeTailor();
     }
 });
+
 //关闭裁剪框
 function closeTailor() {
     $(".tailoring-container").toggle();
